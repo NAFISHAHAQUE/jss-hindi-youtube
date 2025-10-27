34f798c940acbc8adaaf4dd63b8f871472d74235
NAFISHAHAQUE <143682112+NAFISHAHAQUE@users.noreply.github.com>
2025-10-27 12:46:39 +0000

    conversion

--- 01_basics/03_conversionOperators.js
+++ 01_basics/03_conversionOperators.js
@@ -19,4 +19,28 @@ let stringNumber=String(somenumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
-
+//*****************Operations***************************
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+let str1="hello"
+let str2="naf"
+let str3=str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log((5+3)*5%3);
+console.log(+true);
+console.log(+"");
+let num1, num2, num3
+num1=num2=num3=2+2
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
