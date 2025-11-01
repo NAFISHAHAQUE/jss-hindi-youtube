c437b00ebcbcc6d20e06fe3ce46a6955d831b39b
NAFISHAHAQUE <143682112+NAFISHAHAQUE@users.noreply.github.com>
2025-11-01 08:05:38 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,24 @@
+const name="nafisha"
+const repocount=786
+//console.log(name + repocount + "value");
+
+console.log(`hello my name is ${name} and my repocount is ${repocount}`);
+const gameName= new String("nafishadell")
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+const newString=gameName.substring(0,4);
+console.log(newString);
+const anotherString=gameName.slice(-8,4);
+console.log(anotherString);
+const newStringOne="    nafisha    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+const url="http://nafishahaque%786abhirathore"
+console.log(url.replace('%786','_'));
+console.log(url.includes("sundar"));
+console.log(gameName.split('_'));
+
